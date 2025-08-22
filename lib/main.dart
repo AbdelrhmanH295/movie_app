e9d4a072f05d534233e07d2764796780550cf175
Abdelrhman Hammam <abodyhard295@gmail.com>
2025-08-22 14:31:26 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/screens/forget_password.dart';
+import 'package:movie_app/screens/login_screen.dart';
+import 'package:movie_app/screens/on_boarding.dart';
 import 'package:movie_app/screens/profile_update_screen.dart';
 import 'package:movie_app/screens/register_screen.dart';
+import 'package:movie_app/utils/app_routes.dart';
+import 'package:movie_app/screens/register_screen.dart';
+import 'package:movie_app/utils/app_routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,9 +19,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: AppRoutes.onBoarding ,
+      routes: {
+        AppRoutes.loginRouteName: (context) => LoginScreen(),
+        AppRoutes.registerRouteName: (context) => RegisterScreen(),
+        AppRoutes.forgetScreenRouteName: (context) => ForgetPassword(),
+        AppRoutes.updateProfileRouteName: (context) => ProfileUpdateScreen(),
+        AppRoutes.onBoarding: (context) => OnBoarding(),
+      },
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: ForgetPassword(),
+      home:  OnBoarding(),
     );
   }
 }
