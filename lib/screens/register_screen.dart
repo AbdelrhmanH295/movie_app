b484efa312b1798e343486cb9c5057d7d2b84337
Abdelrhman Hammam <abodyhard295@gmail.com>
2025-08-22 18:36:46 +0300

    update

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -27,130 +27,132 @@ class RegisterScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: height * 0.02),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Center(
-              child: Image.asset(
-                AppAssets.registerScreenImage,
-              ),
-            ),
-            SizedBox(
-              height: height * 0.01,
-            ),
-            Text(
-              'Avatar',
-              textAlign: TextAlign.center,
-              style: AppStyles.regular16White,
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: AppColors.darkGreyColor,
-                  borderRadius: BorderRadius.circular(15)),
-              child: CustomTextFormField(
-                colorBorderSide: AppColors.darkGreyColor,
-                hintText: 'Name',
-                hintStyle: AppStyles.regular16White,
-                prefixIcon: Image.asset(AppAssets.usernameIconRegister),
-              ),
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: AppColors.darkGreyColor,
-                  borderRadius: BorderRadius.circular(15)),
-              child: CustomTextFormField(
-                colorBorderSide: AppColors.darkGreyColor,
-                hintText: 'Email',
-                hintStyle: AppStyles.regular16White,
-                prefixIcon: Image.asset(AppAssets.emailIcon),
-              ),
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: AppColors.darkGreyColor,
-                  borderRadius: BorderRadius.circular(15)),
-              child: CustomTextFormField(
-                colorBorderSide: AppColors.darkGreyColor,
-                hintText: 'Password',
-                hintStyle: AppStyles.regular16White,
-                prefixIcon: Image.asset(AppAssets.passwordIcon),
-                suffixIcon: Image.asset(AppAssets.eyeHiddenIcon),
-              ),
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: AppColors.darkGreyColor,
-                  borderRadius: BorderRadius.circular(15)),
-              child: CustomTextFormField(
-                colorBorderSide: AppColors.darkGreyColor,
-                hintText: 'Confirm Password',
-                hintStyle: AppStyles.regular16White,
-                prefixIcon: Image.asset(AppAssets.passwordIcon),
-                suffixIcon: Image.asset(AppAssets.eyeHiddenIcon),
-              ),
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: AppColors.darkGreyColor,
-                  borderRadius: BorderRadius.circular(15)),
-              child: CustomTextFormField(
-                colorBorderSide: AppColors.darkGreyColor,
-                hintText: 'Phone Number',
-                hintStyle: AppStyles.regular16White,
-                prefixIcon: Image.asset(AppAssets.phoneIcon),
-              ),
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            CustomElevatedButton(
-              onPressed: () {
-                //To Do : confirm the accounte and save in fire base
-                Navigator.pop(context);
-              },
-              text: 'Create Accounte',
-              backgroundColor: AppColors.yellowColor,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Already Have Accounte ?',
-                  style: AppStyles.regular14White,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: Image.asset(
+                  AppAssets.registerScreenImage,
                 ),
-                TextButton(
-                    onPressed: () {
-                      //To Do : go to login screen
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      'Login',
-                      style: AppStyles.regular14Yellow,
-                    ))
-              ],
-            ),
-            SizedBox(
-              height: height * 0.02,
-            ),
-            Align(
-                alignment: Alignment.topCenter,
-                child: Image.asset(AppAssets.languageSwitchImage))
-          ],
+              ),
+              SizedBox(
+                height: height * 0.01,
+              ),
+              Text(
+                'Avatar',
+                textAlign: TextAlign.center,
+                style: AppStyles.regular16White,
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: AppColors.darkGreyColor,
+                    borderRadius: BorderRadius.circular(15)),
+                child: CustomTextFormField(
+                  colorBorderSide: AppColors.darkGreyColor,
+                  hintText: 'Name',
+                  hintStyle: AppStyles.regular16White,
+                  prefixIcon: Image.asset(AppAssets.usernameIconRegister),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: AppColors.darkGreyColor,
+                    borderRadius: BorderRadius.circular(15)),
+                child: CustomTextFormField(
+                  colorBorderSide: AppColors.darkGreyColor,
+                  hintText: 'Email',
+                  hintStyle: AppStyles.regular16White,
+                  prefixIcon: Image.asset(AppAssets.emailIcon),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: AppColors.darkGreyColor,
+                    borderRadius: BorderRadius.circular(15)),
+                child: CustomTextFormField(
+                  colorBorderSide: AppColors.darkGreyColor,
+                  hintText: 'Password',
+                  hintStyle: AppStyles.regular16White,
+                  prefixIcon: Image.asset(AppAssets.passwordIcon),
+                  suffixIcon: Image.asset(AppAssets.eyeHiddenIcon),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: AppColors.darkGreyColor,
+                    borderRadius: BorderRadius.circular(15)),
+                child: CustomTextFormField(
+                  colorBorderSide: AppColors.darkGreyColor,
+                  hintText: 'Confirm Password',
+                  hintStyle: AppStyles.regular16White,
+                  prefixIcon: Image.asset(AppAssets.passwordIcon),
+                  suffixIcon: Image.asset(AppAssets.eyeHiddenIcon),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: AppColors.darkGreyColor,
+                    borderRadius: BorderRadius.circular(15)),
+                child: CustomTextFormField(
+                  colorBorderSide: AppColors.darkGreyColor,
+                  hintText: 'Phone Number',
+                  hintStyle: AppStyles.regular16White,
+                  prefixIcon: Image.asset(AppAssets.phoneIcon),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              CustomElevatedButton(
+                onPressed: () {
+                  //To Do : confirm the accounte and save in fire base
+                  Navigator.pop(context);
+                },
+                text: 'Create Accounte',
+                backgroundColor: AppColors.yellowColor,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already Have Accounte ?',
+                    style: AppStyles.regular14White,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        //To Do : go to login screen
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Login',
+                        style: AppStyles.regular14Yellow,
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset(AppAssets.languageSwitchImage))
+            ],
+          ),
         ),
       ),
     );
