4511f3eff7af56dd33b255c11c14b4475029f919
Elzewaidi <mohamed202201048@gmail.com>
2025-08-26 19:20:22 +0300

    +add api_login

---
+++ lib/api/api_login_screen/api_manager.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+// import 'package:movies_app/model/register_response.dart';
+import 'package:movie_app/model/login_response.dart';
+
+import 'api-constant.dart';
+import 'api-endpoints.dart';
+
+class ApiManager {
+  static Future<LoginResponse> login(String email, String password) async {
+    final url = Uri.parse("${ApiConstants.baseUrl}${ApiEndpoints.login}");
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"email": email, "password": password}),
+    );
+
+    final responseData = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return LoginResponse.fromJson(responseData);
+    } else {
+      throw Exception(responseData["message"] ?? "Login failed, try again.");
+    }
+  }
+}
